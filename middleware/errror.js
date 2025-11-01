98196824b5a380c6da326e23c0f4097d419cc87c
sajnahashim <sajnahashim02@gmail.com>
2025-11-01 18:25:36 +0530

    Fix case-sensitivity and clear Git cache

--- middleware/errror.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error('Global Error:', err.stack);
-  res.status(err.statusCode || 500).json({ error: err.message || 'Internal server error' });
-};
-
-module.exports = errorHandler;
