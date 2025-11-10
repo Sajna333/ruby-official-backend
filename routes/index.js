ef0e724868f17db7d0f1ce1225c7104522d2071c
sajnahashim <sajnahashim02@gmail.com>
2025-11-10 20:53:07 +0530

    Remove redundant routes/index.js to avoid conflict with _index_old.js

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("Welcome to Ruby Official API root route");
-});
-
-module.exports = router;
